ec230da4e00189520a31d005cc65748086d692c9
sopy <contact@sopy.one>
2023-08-17 22:59:21 +0300

    Add distance calculation and sorting to GetSpotsHandler
    code committed about a month later because I didn't get a response clarifying what the requirements were as the wording could be interpreted in multiple ways
    
    Enhanced the GetSpotsHandler function in handler.go. It now calculates the distance of each spot from a given point and includes this value in the SQL query result. Added a new struct type Point to hold each spot with its distance. After the query, it sorts the spots - if the spots' distances are less than 50m apart, they get sorted by rating; otherwise, they get sorted by distance. This provides more relevant information to users viewing the spots.

--- handlers/handler.go
+++ handlers/handler.go
@@ -2,6 +2,9 @@ package handler
 
 import (
 	"database/sql"
+	"math"
+	"sort"
+
 	// core packages
 	"encoding/json"
 	"log"
@@ -12,6 +15,11 @@ import (
 	"SpotsTest/database"
 	"SpotsTest/models"
 )
+
+type Point struct {
+	first  models.Spot
+	second float64
+}
 
 func GetSpotsHandler(w http.ResponseWriter, r *http.Request) {
 	// Parse query parameters
@@ -49,14 +57,15 @@ func GetSpotsHandler(w http.ResponseWriter, r *http.Request) {
 				website, 
 				coordinates, 
 				description, 
-				rating
+				rating,
+				st_distance(coordinates, ST_MakePoint($1, $2)::geography) AS distance
 			FROM public."MY_TABLE"
 			WHERE ST_DWithin(
 			    ST_MakePoint($1, $2)::geography, 
 			    coordinates,
 			    $3
-			);
-		`
+			)
+			ORDER BY distance;`
 	} else if recruitmentType == "square" {
 		query = `
 			SELECT
@@ -65,23 +74,26 @@ func GetSpotsHandler(w http.ResponseWriter, r *http.Request) {
 				website,
 				coordinates,
 				description,
-				rating
+				rating,
+				st_distance(coordinates, given_point) AS distance
 			FROM 
 			    public."MY_TABLE",
 			    (SELECT
 					$1::double precision AS long, 
 					$2::double precision AS lat,
-					$3::double precision AS dist, 
+					ST_MakePoint($1, $2)::geography AS given_point,
+					$3::double precision AS radius,
 					111319.0 AS metersPerDegree) AS c
 			WHERE 
 				coordinates::geometry &&
 				ST_MakeEnvelope(
-					long - (dist / (metersPerDegree * COS(RADIANS(lat)))),
-					lat  - (dist / metersPerDegree), 
-					long + (dist / (metersPerDegree * COS(RADIANS(lat)))), 
-					lat  + (dist / metersPerDegree), 
+					long - (radius / (metersPerDegree * COS(RADIANS(lat)))),
+					lat  - (radius / metersPerDegree), 
+					long + (radius / (metersPerDegree * COS(RADIANS(lat)))), 
+					lat  + (radius / metersPerDegree), 
 					4326
-				)`
+				)
+			ORDER BY distance;`
 	} else {
 		http.Error(w, "Invalid recruitment type", http.StatusBadRequest)
 		return
@@ -103,23 +115,41 @@ func GetSpotsHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Create an array to store the spots
 	var spots []models.Spot
+	var pairs []Point
 
 	// Iterate through the result set and populate the spots array
 	for rows.Next() {
 		var spot models.Spot
-		err := rows.Scan(&spot.ID, &spot.Name, &spot.Website, &spot.Coordinates, &spot.Description, &spot.Rating)
+		var point Point
+		err := rows.Scan(&spot.ID, &spot.Name, &spot.Website, &spot.Coordinates, &spot.Description, &spot.Rating, &point.second)
 		if err != nil {
 			http.Error(w, "Error scanning row", http.StatusInternalServerError)
 			log.Println("error scanning row: ", err)
 			return
 		}
-		spots = append(spots, spot)
+		point.first = spot
+		pairs = append(pairs, point)
 	}
 
 	// Check for any errors encountered during iteration
 	if err := rows.Err(); err != nil {
 		http.Error(w, "Error iterating result set", http.StatusInternalServerError)
 		return
+	}
+
+	// sort spots by rating of distance <50m
+	sort.Slice(pairs, func(i, j int) bool {
+		if math.Abs(pairs[i].second-pairs[j].second) < 50 {
+			return pairs[i].first.Rating > pairs[j].first.Rating
+		}
+
+		return pairs[i].second < pairs[j].second
+	})
+
+	// loop through pairs and append to spots
+	for _, pair := range pairs {
+		log.Printf("spot: %v, Dist: %f", pair.first, pair.second)
+		spots = append(spots, pair.first)
 	}
 
 	// Convert the spots array to JSON
